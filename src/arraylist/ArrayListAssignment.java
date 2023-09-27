dcc695f0ac484b6cba306374fb666bd09c517131
Dilipjp <jpdilipk@gmail.com>
2023-09-27 11:52:46 +0530

    Creating an ArrayList

---
+++ src/arraylist/ArrayListAssignment.java
@@ -0,0 +1,21 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListAssignment {
+	
+	ArrayList <Integer> numberList = new ArrayList<Integer>(10);
+	
+	public ArrayListAssignment() {
+		for(int x= 0; x< 10; x++) {
+			numberList.add(x);
+			
+		}
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
